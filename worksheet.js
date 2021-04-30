6cc0a98bfdb461bad7b422d9c053b34401e30c5a
millersteventyler <millersteventyler@gmail.com>
2021-04-30 16:50:36 -0400

    Created arrays for destination, restaurant, transportation and entertainment

---
+++ worksheet.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let destination = ['Michigan', 'South Carolina', 'Texas', 'California', 'Kentucky'];
+let restaurant = ["McDonald's", "Wendy's", "Taco Bell", "Subway", "Arby's"];
+let transportation = ['Car', 'Train', 'Helicopter', 'Jet Ski', 'Lamborfeeties'];
+let entertainment = ['Movie', 'Magic Show', 'Sporting event', 'Casino', 'Concert'];
