1b604fbd4ac63a9e578437e5d35bbe53ffdfecf1
millersteventyler <millersteventyler@gmail.com>
2021-05-01 14:05:55 -0400

    Created randomDestination funtion and set it to print to console. lines 3-8

--- worksheet.js
+++ worksheet.js
@@ -1,6 +1,12 @@
 "use strict";
 
+function randomDestination(place){
+    return place[Math.floor(Math.random()*place.length)]
+}
+
 let destination = ['Michigan', 'South Carolina', 'Texas', 'California', 'Kentucky'];
+console.log(randomDestination(destination));
+
 let restaurant = ["McDonald's", "Wendy's", "Taco Bell", "Subway", "Arby's"];
 let transportation = ['Car', 'Train', 'Helicopter', 'Jet Ski', 'Lamborfeeties'];
-let entertainment = ['Movie', 'Magic Show', 'Sporting event', 'Casino', 'Concert'];
+let entertainment = ['Movie', 'Magic Show', 'Sporting event', 'Casino', 'Concert']
