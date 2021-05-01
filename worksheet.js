2038eb0dc71109565e351e76af955e1e8f1b22dc
millersteventyler <millersteventyler@gmail.com>
2021-05-01 14:17:36 -0400

    Created randomRestaurant function and printed to console. lines 10-15

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,21 @@
 "use strict";
 
 function randomDestination(place){
-    return place[Math.floor(Math.random()*place.length)]
+    return place[Math.floor(Math.random()*place.length)];
 }
 
 let destination = ['Michigan', 'South Carolina', 'Texas', 'California', 'Kentucky'];
 console.log(randomDestination(destination));
 
+function randomRestaurant(food){
+    return food[Math.floor(Math.random()*food.length)];
+}
+
 let restaurant = ["McDonald's", "Wendy's", "Taco Bell", "Subway", "Arby's"];
+console.log(randomRestaurant(restaurant));
+
+
+
+
 let transportation = ['Car', 'Train', 'Helicopter', 'Jet Ski', 'Lamborfeeties'];
 let entertainment = ['Movie', 'Magic Show', 'Sporting event', 'Casino', 'Concert']
